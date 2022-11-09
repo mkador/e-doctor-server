40b38af43a2171a26411ef5defff56f66fc40644
Mk Adore <mkador169@gmil.com>
2022-11-09 22:51:30 +0600

    add service api added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 const cors = require('cors')
 const port = process.env.PORT || 5000
 require('dotenv').config()
@@ -8,15 +8,49 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
-console.log(process.env.DB_USER)
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.1g0ejju.mongodb.net/?retryWrites=true&w=majority`
-console.log(uri)
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 })
+
+async function run() {
+  try {
+    const servicesCollection = client.db('e-doctor').collection('services')
+    /// only three services in home page
+    app.get('/services', async (req, res) => {
+      const query = {}
+      const cursor = servicesCollection.find(query)
+      const services = await cursor.limit(3).toArray()
+      res.send(services)
+    })
+
+    ////all services api to show all service in all services section
+    app.get('/allServices', async (req, res) => {
+      const query = {}
+      const cursor = servicesCollection.find(query)
+      const services = await cursor.toArray()
+      res.send(services)
+    })
+    ///Single service with details api
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: ObjectId(id) }
+      const service = await servicesCollection.findOne(query)
+      res.send(service)
+    })
+
+    /// for adding service api
+    app.post('/allServices', async (req, res) => {
+      const addService = req.body
+      const result = await servicesCollection.insertOne(addService)
+      res.send(result)
+    })
+  } finally {
+  }
+}
+run().catch((error) => console.error(error))
 
 app.get('/', (req, res) => {
   res.send('e-doctor server is Running')
