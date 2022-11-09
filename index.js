54a6829cb5f350a4790908c94d25e88bec834c9d
Mk Adore <mkador169@gmil.com>
2022-11-09 12:12:30 +0600

    environment set setup

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb')
 const cors = require('cors')
 const port = process.env.PORT || 5000
+require('dotenv').config()
 
 app.use(cors())
 app.use(express.json())
+
+console.log(process.env.DB_USER)
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.1g0ejju.mongodb.net/?retryWrites=true&w=majority`
+console.log(uri)
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+})
 
 app.get('/', (req, res) => {
   res.send('e-doctor server is Running')
